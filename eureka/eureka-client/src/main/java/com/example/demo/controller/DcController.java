a36a0ffd1effd9a7d3a2f369e110cd277d023cb6
designinn <paranoia_zk@yeah.net>
2017-12-13 16:07:50 +0800

    服务注册与发现

---
+++ eureka/eureka-client/src/main/java/com/example/demo/controller/DcController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author PARANOIA_ZK
+ * @date 2017/12/13 14:34
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+}
