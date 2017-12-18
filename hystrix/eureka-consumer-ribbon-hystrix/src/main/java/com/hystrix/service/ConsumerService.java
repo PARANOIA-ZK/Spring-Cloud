c609af4a2cd1d91eb499b3b76a89defc32263954
designinn <paranoia_zk@yeah.net>
2017-12-18 17:12:09 +0800

    Hystrix监控面板【Dalston版】

--- hystrix/eureka-consumer-ribbon-hystrix/src/main/java/com/hystrix/service/ConsumerService.java
+++ hystrix/eureka-consumer-ribbon-hystrix/src/main/java/com/hystrix/service/ConsumerService.java
@@ -15,6 +15,11 @@ public class ConsumerService {
     @Autowired
     RestTemplate restTemplate;
 
+    /**
+     * 我们使用了@HystrixCommand来将某个函数包装成了Hystrix命令，这里除了定义服务降级之外，Hystrix框架就会自动的为这个函数实现调用的隔离。
+     * 所以，依赖隔离、服务降级在使用时候都是一体化实现的，这样利用Hystrix来实现服务容错保护在编程模型上就非常方便的，并且考虑更为全面。
+     * @return
+     */
     @HystrixCommand(fallbackMethod = "fallback")
     public String consumer() {
         return restTemplate.getForObject("http://eureka-client/dc", String.class);
