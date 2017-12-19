b7e4f55799ea051abc6d03b36289abe1b325d52f
designinn <paranoia_zk@yeah.net>
2017-12-19 15:31:50 +0800

    服务网关（过滤器）

--- api/api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
+++ api/api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  *  http://blog.didispace.com/spring-cloud-starter-dalston-6-1/
@@ -14,4 +16,9 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 }
