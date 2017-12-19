52697d9217ea03f527823eded61078dbd4a2fc41
designinn <paranoia_zk@yeah.net>
2017-12-19 13:32:49 +0800

    服务网关（基础）【Dalston版】

---
+++ api/api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ *  http://blog.didispace.com/spring-cloud-starter-dalston-6-1/
+ */
+@SpringCloudApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
